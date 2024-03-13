499aaa65c453e14da4cdf5341560a115b27a692d
it-unit-2024 <it-unit-2024@gmail.com>
2024-03-13 08:56:01 +0400

    part 7 done

--- end.js
+++ end.js
@@ -2,16 +2,16 @@ const username = document.getElementById("username");
 const saveScoreBtn = document.getElementById("saveScoreBtn");
 const finalScore = document.getElementById("finalScore");
 const mostRecentScore = localStorage.getItem("mostRecentScore");
-// finalScore.innerText = mostRecentScore;
+finalScore.innerText = mostRecentScore;
 // finalScore.innerText = 123;
 
 // localStorage.setItem("mostRecentScore", 159);
 
-if (mostRecentScore != null) {
-  console.log(mostRecentScore);
-} else {
-  console.log("empty");
-}
+// if (mostRecentScore != null) {
+//   console.log(mostRecentScore);
+// } else {
+//   console.log("empty");
+// }
 
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
